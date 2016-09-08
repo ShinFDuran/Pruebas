4984df17a1bbc2ae553dc07c7d81d90d6d839875
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-08 11:18:02 +0200

    C_Meteor - Cambio carga inicial y añadido borrado de imágenes

--- Back-end/Frameworks/Meteor/C_Meteor/image_share/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor/image_share/client/main.js
@@ -1,24 +1,11 @@
 import { Template } from 'meteor/templating';
-
 import './main.html';
 
+Images = new Mongo.Collection('images');
 
-let imgData = [
-  {
-    img_src: 'laptops.jpg',
-    img_alt: 'some laptops on a table',
-  },
-  {
-    img_src: 'bass.jpg',
-    img_alt: 'a bass player',
-  },
-  {
-    img_src: 'beard.jpg',
-    img_alt: 'some musicians with beards',
-  },
-];
 
-Template.images.helpers({ images: imgData });
+// Enviamos la colección Images a la plantilla a través de un helper
+Template.images.helpers({ images: Images.find() });
 
 /**
  * Agregada la clase js-image al listado de imágenes
@@ -28,5 +15,15 @@ Template.images.helpers({ images: imgData });
 Template.images.events({
   'click .js-image': (event) => {
     $(event.target).css('width', '50px');
+  },
+  /**
+   * Para que funcione tenemos que usar la definición antigua de function
+   * this: Objeto de la imagen
+   * this (=>): windows
+   */
+  'click .js-del-image': function (event) {
+    let imageId = this._id;
+    console.log(imageId);
+    $(`#${imageId}`).hide('slow', () => { Images.remove({ _id: imageId }); });
   },
 });
--- Back-end/Frameworks/Meteor/C_Meteor/image_share/server/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor/image_share/server/main.js
@@ -1,14 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
-Images = new Mongo.Collection("images");
-console.log(Images.find().count());
+Images = new Mongo.Collection('images');
 
 Meteor.startup(() => {
   if (Images.find().count() === 0) {
     for (let i = 1; i < 23; i++) {
       Images.insert({
-        img_src: 'img_${{i}}.jpg',
-        img_alt: 'image number ${{i}}',
+        img_src: `img_${i}.jpg`,
+        img_alt: `image number ${i}`,
       });
     }// end of for insert images
     // count the images!
