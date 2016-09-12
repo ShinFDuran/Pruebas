37a036425de2dc83aada2ee31f0ea0d830eafc26
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-13 00:15:20 +0200

    C_Meteor2 - Código inicial de la aplicación escribiendo código simultáneo

--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
@@ -1,2 +1,13 @@
 import { Template } from 'meteor/templating';
 import './main.html';
+
+Template.editor.helpers({
+  docid: () => {
+    console.log(`Doc id helper: ${Documents.findOne()}`);
+    const doc = Documents.findOne();
+    if (doc) {
+      return doc._id;
+    }
+    return undefined;
+  },
+});
--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/server/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/server/main.js
@@ -2,4 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (!Documents.findOne()) {
+    // There is no documents yet, we need to initialize
+    Documents.insert({ title: 'My new document' });
+  }
 });
