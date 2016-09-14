f44c4fbb033b4a8dcf12610beb1c959b0a9d7834
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-14 11:14:58 +0200

    C_Meteor2 - Agregado Bootstrap al proyecto

--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
@@ -1,13 +1,5 @@
 import { Template } from 'meteor/templating';
 
-// The time will be updated each second 
-Meteor.setInterval(() => {
-  Session.set('current_date', new Date());
-}, 1000);
-
-Template.date_display.helpers({
-  current_date: () => Session.get('current_date'),
-});
 
 Template.editor.helpers({
   docid: () => {
