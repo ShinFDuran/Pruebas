5d5d84c18265f6b258d68efe9db19cfba15f9912
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-14 11:56:45 +0200

    C_Meteor2 - Agregado el visor de html

--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
@@ -3,11 +3,21 @@ import { Template } from 'meteor/templating';
 
 Template.editor.helpers({
   docid: () => {
-    const doc = Documents.findOne();
+    let doc = Documents.findOne();
     console.log(`Doc id helper: ${doc}`);
     if (doc) {
       return doc._id;
     }
     return undefined;
   },
+  config: () =>
+    editor => {
+      editor.on('change', (cmEditor, info) => {
+        // The content of the text editor is in cmEditor.getValue()
+        const content = cmEditor.getValue();
+        console.log(content);
+        // We use jQuery to get the viewer and add it the content
+        $('#viewer_iframe').contents().find('html').html(content);
+      });
+    },
 });
