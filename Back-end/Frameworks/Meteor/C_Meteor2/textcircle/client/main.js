6a610cb91ff0029a930132644fd05c63f10485f3
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-14 10:07:29 +0200

    C_Meteor2 - Agregado date display

--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
@@ -1,10 +1,18 @@
 import { Template } from 'meteor/templating';
-import './main.html';
+
+// The time will be updated each second 
+Meteor.setInterval(() => {
+  Session.set('current_date', new Date());
+}, 1000);
+
+Template.date_display.helpers({
+  current_date: () => Session.get('current_date'),
+});
 
 Template.editor.helpers({
   docid: () => {
-    console.log(`Doc id helper: ${Documents.findOne()}`);
     const doc = Documents.findOne();
+    console.log(`Doc id helper: ${doc}`);
     if (doc) {
       return doc._id;
     }
