01afa76ffef518f1d0f76841df8866a6d3cdf024
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-20 16:59:49 +0200

    C_Meteor2 - Agregado lista con los editores del documento

--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/client/main.js
@@ -12,12 +12,51 @@ Template.editor.helpers({
   },
   config: () =>
     editor => {
+      editor.setOption('lineNumbers', true);
+      editor.setOption('theme', 'cobalt');
       editor.on('change', (cmEditor, info) => {
         // The content of the text editor is in cmEditor.getValue()
         const content = cmEditor.getValue();
         console.log(content);
         // We use jQuery to get the viewer and add it the content
         $('#viewer_iframe').contents().find('html').html(content);
+        Meteor.call('addEditingUser');
       });
     },
 });
+
+Template.editingUsers.helpers({
+  // retrieve a set of users that are editing this document
+  users: () => {
+    let eusers;
+    let users;
+    let doc = Documents.findOne();
+    // no doc -> give up
+    if (!doc) {
+      return;
+    }
+    eusers = EditingUsers.findOne({ docid: doc._id });
+    // No logged in user -> give up
+    if (!eusers) {
+      return;
+    }
+    users = new Array();
+    let i = 0;
+    for (let user_id in eusers.users) {
+      users[i] = fixObjectKeys(eusers.users[user_id]);
+      i++;
+    }
+    return users;
+  },
+});
+
+// Helper function
+// Renames object keys by removing hyphens to make the compatible with spacebars
+function fixObjectKeys(obj) {
+  let newObj = {};
+  for (let key in obj) {
+    let key2 = key.replace("-", "");
+    newObj[key2] = obj[key];
+  }
+  return newObj;
+}
--- Back-end/Frameworks/Meteor/C_Meteor2/textcircle/lib/collections.js
+++ Back-end/Frameworks/Meteor/C_Meteor2/textcircle/lib/collections.js
@@ -1,1 +1,37 @@
 this.Documents = new Mongo.Collection('documents');
+EditingUsers = new Mongo.Collection('editingUsers');
+
+// Methods that provide write access to the data
+Meteor.methods({
+  // Allows changes to the editing users collection
+  addEditingUser: function () {
+    let user;
+    let eusers;
+    let doc = Documents.findOne();
+    // no doc -> give up
+    if (!doc) {
+      return;
+    }
+    // No logged in user -> give up
+    if (!this.userId) {
+      return;
+    }
+    // Now I have a doc, and possibly a user
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({
+      docid: doc._id
+    });
+    // If no editing users have been stored yet
+    if (!eusers) {
+      // We create the object of the doc with an empty users object
+      eusers = {
+        docid: doc._id,
+        users: {},
+      };
+    }
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    // Upsert- insert or update if filter matches
+    EditingUsers.upsert({ _id: eusers._id }, eusers);
+  },
+});
