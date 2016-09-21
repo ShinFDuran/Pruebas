c6a548ff92a71e60dfde4650d7789c53bc45e9c8
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-09-21 16:27:02 +0200

    EDX_MEAN - HW1 solucionado

--- Full-Stack/MEAN/EDX_MEAN/hw1/interface.js
+++ Full-Stack/MEAN/EDX_MEAN/hw1/interface.js
@@ -3,6 +3,8 @@
  */
 exports.insert = function(db, doc, callback) {
   // TODO: implement
+  // We only need to insert a documento in the collection
+  db.collection('movies').insert(doc);
   callback(null);
 };
 
@@ -14,5 +16,11 @@ exports.insert = function(db, doc, callback) {
  */
 exports.byDirector = function(db, director, callback) {
   // TODO: implement
-  callback(null, []);
+  // We need to use find with the director that cames in the function
+  // We will sort by title and ascending (1)
+  db.collection('movies').find({ director }).sort({ title: 1 })
+    .toArray(function(error, docs) {
+      // Finally we access to the array of documents in the callback
+      callback(null, docs);
+    });
 };
