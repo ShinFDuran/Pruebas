b211cb26dc7ba147ce2e65448746fc2febab5c9d
Francisco Durán <franciscodurannavarro@hotmail.com>
2016-10-14 13:20:53 +0200

    A2_APM - Added custom pipe

--- Front-end/Frameworks/Angular2/APM/app/products/product-list.component.ts
+++ Front-end/Frameworks/Angular2/APM/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from 'angular2/core';
 
-
 import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe';
 
 @Component({
   selector: 'pm-products',
   templateUrl: 'app/products/product-list.component.html',
+  pipes: [ProductFilterPipe],
 })
 
 export class ProductListComponent implements OnInit {
