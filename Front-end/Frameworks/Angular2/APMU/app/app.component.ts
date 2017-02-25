a6091aedcb3729b966948c281277c8bf313b8cb1
Francisco Durán <franciscodurannavarro@hotmail.com>
2017-02-25 12:24:17 +0100

    A2_APMU - First component

--- Front-end/Frameworks/Angular2/APMU/app/app.component.ts
+++ Front-end/Frameworks/Angular2/APMU/app/app.component.ts
@@ -1,9 +1,20 @@
+// Angular 2 Component Structure 
+// Imports of the needed modules
 import { Component } from '@angular/core';
 
+// Decorator with the data needed for the component (metadata)
 @Component({
+    // selector: Directive in html
     selector: 'pm-app',
+    // View layout of the component. Can be a link to a external html
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <div><h1>{{pageTitle}}</h1>
+            <h2>My first component</h2>
+        </div>
     `
 })
-export class AppComponent { }
+
+// Class with the data (properties) and logic (methods) of the component
+export class AppComponent {
+    pageTitle: string = 'Acme Product Manager';
+}
