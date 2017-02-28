33d06b84fb7a637eb0f1bf046adccc1080f75bc6
Francisco Durán <franciscodurannavarro@hotmail.com>
2017-02-28 12:56:28 +0100

    A_CWUAQ - Use of simple bindings
    Interpolation, property binding and event binding

--- Front-end/Frameworks/Angular/CWUAQ/src/app/app.component.ts
+++ Front-end/Frameworks/Angular/CWUAQ/src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',  // directive <my-app>
-  template: `<h1>Hello {{name}}</h1>`,
+  moduleId: module.id,
+  templateUrl: 'app.component.html'
 })
 export class AppComponent  { 
   // Properties
   name = 'Angular'; 
+  image = 'favicon.ico';
+  color = 'red';
+
+  // Methods
+  clicked(){
+    // We will use a ternary if: condición ? expr1 : expr2 
+    this.color = this.color === 'red' ? 'blue' : 'red';
+  }
+
+  colorChange(color: string){
+    this.color = color;
+  }
 }
