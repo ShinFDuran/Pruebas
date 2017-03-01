58d13a7e6bee2f97de27065120ce9b629bcaee0b
Francisco Durán <franciscodurannavarro@hotmail.com>
2017-03-01 17:48:52 +0100

    A_CWUAQ - Changed customer to hero and hero list added

--- Front-end/Frameworks/Angular/CWUAQ/src/app/app.component.ts
+++ Front-end/Frameworks/Angular/CWUAQ/src/app/app.component.ts
@@ -1,25 +1,57 @@
 import { Component } from '@angular/core';
 
-import { Customer } from './model';
+import { Hero } from './model';
 
 @Component({
   selector: 'my-app',  // directive <my-app>
   moduleId: module.id,
   templateUrl: 'app.component.html'
 })
+
 export class AppComponent  { 
   // Properties
-  pageTitle: string = 'Code Quickstart';
-  pageDescription: string = 'Example web page using Angular Quickstart';
-  hideAdress: boolean = true;
-  customer: Customer = {
-    id: 1,
-    name: 'Batman',
-    address: {
-      street: 'Falsa 123',
-      city: 'Gotham',
-      state: 'Chicago',
-      region: 'West'
-    }
-  };
+  pageTitle: string = 'Hero Quickstart';
+  pageDescription: string;
+  hideAdress: boolean;
+  regions: string[];
+  states: string[];
+  heroes: Hero[];
+  hero: Hero;
+
+  // Constructor.  Properties initialization
+  constructor(){
+    this.pageTitle = 'Hero Quickstart';
+    this.pageDescription = 'Example web page using Angular Quickstart';
+    this.hideAdress = true;
+    this.regions = ['north', 'south', 'west', 'east'];
+    this.states = ['Chicago','New York','Alaska','Jalisco'];
+    this.heroes = [{
+      id: 1,
+      name: 'Batman',
+      address: {
+        street: 'Wayne Manor',
+        city: 'Gotham',
+        state: 'Chicago',
+        region: 'west'
+      }
+    },{
+      id: 2,
+      name: 'Spiderman',
+      address: {
+        street: 'Daily Bugle',
+        city: 'New York',
+        state: 'New York',
+        region: 'east'
+      }
+    },{
+      id: 3,
+      name: 'Superman',
+      address: {
+        street: 'Daily Planet',
+        city: 'Metropolis',
+        state: 'Alaska',
+        region: 'north'
+      }
+    }];
+  }
 }
