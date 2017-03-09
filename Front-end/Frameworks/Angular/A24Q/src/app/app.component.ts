4801b5575730d03ebaf0b651455b5fb321774a1a
Francisco Durán <franciscodurannavarro@hotmail.com>
2017-03-09 17:01:07 +0100

    A_A24Q - Mostrar listado de héroes

--- Front-end/Frameworks/Angular/A24Q/src/app/app.component.ts
+++ Front-end/Frameworks/Angular/A24Q/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { Heroe } from './app.heroe.model';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
   template: `
   <h1>{{titulo}}</h1>
@@ -14,7 +15,14 @@ import { Heroe } from './app.heroe.model';
     <label>Nombre: </label>
     <input [(ngModel)]="heroe.name" placeholder="Nombre héroe">
   </div>
+  <h2>Mis Héroes</h2>
+  <ul class="heroes">
+    <li *ngFor="let heroe of HEROES">
+      <span class="badge">{{heroe.id}}</span> {{heroe.name}}
+    </li>
+  </ul>
   `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
   titulo: string;
@@ -22,7 +30,21 @@ export class AppComponent  implements OnInit {
 
   ngOnInit(){
     this.titulo = 'Tour de Héroes';
-    this.heroe = new Heroe(1,'Shadow');
+    this.heroe = this.HEROES[0];
   }
+
+  // Listado manual de Héroes
+  const HEROES: Hero[] = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
 
 }
